925bf1fcb9b6857be6d1ad4dbd151db19dbd89ac
Johannes Stadter <info@stadter-kamin.de>
2026-09-17 11:20:00 +0200

    Barrierefreiheit und Formularversand verbessert

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
 import node from '@astrojs/node';
 
-// https://astro.build/config
 export default defineConfig({
   adapter: node({
     mode: 'standalone'
-  })
+  }),
+
+  security: {
+    checkOrigin: false
+  }
 });
